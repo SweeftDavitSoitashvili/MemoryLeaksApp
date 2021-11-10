dec654e640f4136a4b1773971356ef02812059a6
Davit Soitashvili <davitsoitashvili@Davits-MacBook-Pro.local>
2021-11-10 15:38:44 +0400

    inner class memory leak case

--- app/src/main/java/com/example/memoryleaksapp/SecondActivity.kt
+++ app/src/main/java/com/example/memoryleaksapp/SecondActivity.kt
@@ -2,24 +2,33 @@ package com.example.memoryleaksapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log.d
 import android.widget.TextView
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
-    // Causes memory leak
-    companion object {
-        private lateinit var secondActivityTextView : TextView
-    }
+    private lateinit var secondActivityTextView : TextView
 
-    // Fix the memory leak
-    // private lateinit var secondActivityTextView : TextView
+    companion object {
+        private lateinit var testInnerClass: TestInnerClass
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        initViews()
+        init()
+        testInnerClass.printMessage()
     }
 
-    private fun initViews() {
+    private fun init() {
         secondActivityTextView = findViewById(R.id.secondActivityTextView)
+        testInnerClass = TestInnerClass()
+    }
+
+    //Cases Memory leak
+    inner class TestInnerClass {
+        fun printMessage() {
+            d("LogPrintedMessage", secondActivityTextView.text.toString())
+        }
     }
 }
