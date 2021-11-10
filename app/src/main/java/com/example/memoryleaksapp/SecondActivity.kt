ace83b915c45dd957b903b8a1e39e367e00905e8
Davit Soitashvili <davitsoitashvili@Davits-MacBook-Pro.local>
2021-11-10 16:25:59 +0400

    pass reference in background worker memory leak case

--- app/src/main/java/com/example/memoryleaksapp/SecondActivity.kt
+++ app/src/main/java/com/example/memoryleaksapp/SecondActivity.kt
@@ -3,32 +3,46 @@ package com.example.memoryleaksapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log.d
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class SecondActivity : AppCompatActivity() {
 
     private lateinit var secondActivityTextView : TextView
-
-    companion object {
-        private lateinit var testInterface : TestInterface
-    }
+    private lateinit var showToastMessageBtn : Button
+    private lateinit var testThread: TestThread
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         init()
-        testInterface.printMessage(secondActivityTextView.text.toString())
     }
 
     private fun init() {
         secondActivityTextView = findViewById(R.id.secondActivityTextView)
-        testInterface = TestInterfaceImpl()
+        showToastMessageBtn = findViewById(R.id.showToastMessageBtn)
+        displayMessage(secondActivityTextView.text.toString())
+        testThread = TestThread(this)
+        testThread.start()
     }
 
-    // fix memory leak
-    class TestInterfaceImpl : TestInterface {
-        override fun printMessage(message: String) {
+    private fun makeFakeApiCall() {
+        Thread.sleep(10000)
+        d("LogPrintedMessage", "Response")
+    }
+
+    private fun displayMessage(message : String) {
+        showToastMessageBtn.setOnClickListener {
             d("LogPrintedMessage", message)
+        }
+    }
+
+    // memory leak case (pass strong reference)
+    class TestThread(var secondActivity: SecondActivity) : Thread() {
+        override fun run() {
+            super.run()
+            secondActivity.makeFakeApiCall()
         }
     }
 }
--- app/src/main/java/com/example/memoryleaksapp/TestInterface.kt
+++
@@ -1,5 +0,0 @@
-package com.example.memoryleaksapp
-
-interface TestInterface {
-    fun printMessage(message : String)
-}
